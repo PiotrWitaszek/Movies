8b186e7857e2d3d762c45da0e424003d30e4ec75
Piotr Witaszek <p.a.witaszek@gmail.com>
2022-08-14 12:54:47 +0200

    Few questions and elem in list

--- movies.py
+++ movies.py
@@ -16,51 +16,57 @@ class Series(Movies):
             super().__init__(*args, **kwargs)
             self.season = season
             self.episode = episode        
-        def __str__(self):
+        def __repr__(self):
           return f"{self.title} S{int(self.season):02d}E{int(self.episode):02d}"
 
 def search(list, value):
-  for i in range(0, len(list)):
-    if list[i] == value:
-      print(value)
+  for elem in list:
+    if elem == value:
+      return print(value)
   return False
-
+#czy zamiast value ma być coś w stylu self.title?
 def get_series():
-  for i in range(0, len(list)):
-    if isinstance(list[i], Series):
-      series_list.append(list[i])
+  for elem in list:
+    if isinstance(elem, Series):
+      series_list.append(elem)
+  return series_list
 
 def get_movies():
-  for i in range(0, len(list)):
-    if isinstance(list[i], Series) == False:
-       movies_list.append(list[i])
+  for elem in list:
+    if isinstance(elem, Series) == False:
+       movies_list.append(elem)
+  return movies_list
+#if elem(type(Movies)) == True:
+ #      mniej więcej tak wykorzystać tego type() - wiem, że mniej bo nie wychodzi
+  
       
   
 if __name__ == '__main__':
-    list = [] 
-    series_list = []
-    movies_list = []
-    Shawshank = Movies("Shawshank redemption", "1994", "Drama", "22")
-    list.append(Shawshank)
-    Godfather = Movies("The Godfather", "1972", "Drama", "24")
-    list.append(Godfather)
-    Psycho = Movies("Psycho", "1960", "Thriller", "19")
-    list.append(Psycho)
-    Requiem = Movies("Requiem for a dream", "2000", "Drama", "14")
-    list.append(Requiem) 
-    Peaky = Series("1", "4", "Peaky Blinders", "2013", "Drama", "22")
-    list.append(Peaky)
-    Bad = Series("3", "9", "Breaking Bad", "2008", "Drama", "7")
-    list.append(Bad)
-#wszystko poniżej celem sprawdzenia kodu - wrzucać to też w  maine?
-get_movies()
-search(list, Bad)
-get_series()
-print(series_list)
-print(movies_list)
+  list = [] 
+  series_list = []
+  movies_list = []
+  Shawshank = Movies("Shawshank redemption", "1994", "Drama", "22")
+  list.append(Shawshank)
+  # snake_case chodzi o cos takiego shawshank_redemption?
+  Godfather = Movies("The Godfather", "1972", "Drama", "24")
+  list.append(Godfather)
+  Psycho = Movies("Psycho", "1960", "Thriller", "19")
+  list.append(Psycho)
+  Requiem = Movies("Requiem for a dream", "2000", "Drama", "14")
+  list.append(Requiem) 
+  Peaky = Series("1", "4", "Peaky Blinders", "2013", "Drama", "22")
+  list.append(Peaky)
+  Bad = Series("3", "9", "Breaking Bad", "2008", "Drama", "7")
+  list.append(Bad)
 
-print(Godfather)
-print(Peaky)
-Peaky.play()
-print(Peaky.views)
-print(list)
+  search(list, Bad)
+  get_series()
+  print(series_list)
+  get_movies()
+  print(movies_list)
+
+  print(Godfather)
+  print(Peaky)
+  Peaky.play()
+  print(Peaky.views)
+  print(list)
